364acbcd2c2a6977a36779f379971995c2af5188
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2018-01-14 21:49:41 -0200

    Developing

--- src/main/java/com/alexandreesl/consumer/MyConsumer.java
+++ src/main/java/com/alexandreesl/consumer/MyConsumer.java
@@ -3,10 +3,13 @@ package com.alexandreesl.consumer;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 
 public class MyConsumer {
 
@@ -31,6 +34,7 @@ public class MyConsumer {
     kafkaProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     kafkaProps
         .put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    kafkaProps.put("auto.commit.offset", "false");
     consumer = new KafkaConsumer<String, String>(kafkaProps);
 
   }
@@ -42,10 +46,22 @@ public class MyConsumer {
     try {
       while (true) {
         ConsumerRecords<String, String> records = consumer.poll(100);
+
         for (ConsumerRecord<String, String> record : records) {
           System.out.println("Key: " + record.key());
           System.out.println("Value: " + record.value());
+
+          HashMap<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>();
+
+          offsets.put(new TopicPartition(record.topic(), record.partition()),
+              new OffsetAndMetadata(record.offset() + 1, "no metadata"));
+
+          consumer.commitAsync(offsets, null);
+
+
         }
+
+
       }
     } finally {
       consumer.close();
